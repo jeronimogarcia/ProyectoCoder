baa216dd50d83c296c50c2681727b09669f1ef95
Max Saravia <saravia.max.stk2@gmail.com>
2022-06-05 15:49:54 -0300

    added: footer
    added: footer

---
+++ src/Components/Footer/FooterStyle.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const FooterStyle = styled.footer`
+  height: 147px;
+  background-color: #2D1031;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  #footer-img{
+    height: 58px;
+    width: 150px;
+  }
+`
+
+export default FooterStyle
