eb1359e8e33868ab4a32cedb052dc34eb54cba58
jeronimogarcia <jero_garcia23@hotmail.com>
2022-06-29 21:01:03 -0300

    TitleMediaQuery

--- src/Components/Title/Text.js
+++ src/Components/Title/Text.js
@@ -3,33 +3,35 @@ import styles from "./Title.module.css";
 
 const Text = () => {
   return (
-    <div className={styles.TitleContainer}>
-      <div className={styles.UpContainer}>
-        <span className={styles.Font}>Somos</span>
-        <div className={styles.FlipCard}>
-          <div className={styles.FlipInner}>
-            <span className={`${styles.UpFlipCardFront} ${styles.Font}`}>
-              profesionales
-            </span>
-            <span className={`${styles.UpFlipCardBack} ${styles.Font}`}>
-              fanáticos
-            </span>
+    <div className={styles.TitleMainContainer}>
+      <div className={styles.TitleContainer}>
+        <div className={styles.UpContainer}>
+          <span className={styles.Font}>Somos</span>
+          <div className={styles.FlipCard}>
+            <div className={styles.FlipInner}>
+              <span className={`${styles.UpFlipCardFront} ${styles.Font}`}>
+                profesionales
+              </span>
+              <span className={`${styles.UpFlipCardBack} ${styles.Font}`}>
+                fanáticos
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles.BottomContainer}>
-        <div className={styles.FlipCard}>
-          <div className={styles.FlipInner}>
-            <span className={`${styles.BottomFlipCardFront} ${styles.Font}`}>
-              en
-            </span>
-            <span className={`${styles.BottomFlipCardBack} ${styles.Font}`}>
-              de
-            </span>
+        <div className={styles.BottomContainer}>
+          <div className={styles.FlipCard}>
+            <div className={styles.FlipInner}>
+              <span className={`${styles.BottomFlipCardFront} ${styles.Font}`}>
+                en
+              </span>
+              <span className={`${styles.BottomFlipCardBack} ${styles.Font}`}>
+                de
+              </span>
+            </div>
           </div>
-        </div>
-        <div>
-          <span className={styles.Font}>lo que hacemos</span>
+          <div>
+            <span className={`${styles.Font} ${styles.BottomText}` }>lo que hacemos</span>
+          </div>
         </div>
       </div>
     </div>
